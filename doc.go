45ac8547c95b23f6ac537109cb37348c8a766f69
Felix Jancso-Szabo <felix.ajs@gmail.com>
2019-04-10 23:00:59 -0700

    Add basic docs

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package multiglob implements a multi-pattern glob matcher.
+
+It accepts multiple glob-based patterns and produces a matcher that determines which,
+if any pattern matches. It's radix tree based for maximal efficiency.
+*/
+package multiglob
